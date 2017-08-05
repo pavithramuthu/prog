5483ad39ffe19b4c7194ab38661c07a82dedf374
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2017-08-05 16:58:48 +0530

    even.c

---
+++ odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a
+clrscr();
+printf("Enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the num is even");
+}
+else
+{
+printf("num is odd");
+}
+getch();
+}
