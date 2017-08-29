255a129b749e75487abe013a6face29b519036ba
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2017-08-29 15:01:18 +0530

    vowel

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("Enter the character:");
+scanf("%c",&c);
+if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
+{
+printf("This is a vowel");
+}
+else
+{
+printf("This is not a vowel");
+}
+getch();
+}
