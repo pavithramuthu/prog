5ee1e480a66a506f8c542dbeb14ce7f27ccf877e
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-10 16:50:37 +0530

    palindrome
    pp

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int re,rn=0,n;
+printf("enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+	re=n%10;
+	rn=rn*10+re;
+	n=n/10;
+	if(n==rn)
+	printf("palindrome");
+	else
+	printf("not a palindrome");
+}
+	return n;
+}
