64b46295a23fdb281010c554391b80eb4fa46e74
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-17 16:08:09 +0530

    Update oddrange.c

--- oddrange.c
+++ oddrange.c
@@ -2,17 +2,16 @@
 #include<conio.h>
 void main()
 {
-    int i, n,m;
+    int i,n,m;
     clrscr();
-      printf("Print all even from %d to till %d: ");
+      printf("Print all even from %d to till %d:");
     scanf("%d%d",&m,&n);
-  printf("All even numbers from %d to %d are: \n",m, n);
-   i=1;
-    while(i<=n)
+  printf("All even numbers from %d to %d are:\n",m,n);
+   for(i=0;i<n;i++)
     {
                if(i%2!=0)
         {
-            printf("%d\n", i);
+            printf("%d\n",i);
         }
 
         i++;
