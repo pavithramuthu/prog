eeeb4f043188481fd690497205cd850c09bf19da
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-17 15:58:29 +0530

    Update arm.c

--- arm.c
+++ arm.c
@@ -4,9 +4,9 @@ main()
 int n,rem,result=0,orgn;
 printf("enter the number");
 scanf("%d",&n);
-rem=n%10;
+rem=orgn%10;
 result=result+rem*rem*rem;
-n=n/10;
+orgn=orgn/10;
 if(result==n)
 {
 printf("aemstrong");
