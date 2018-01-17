f32415475fefdfa77d2a19545eb2458437783f42
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-17 16:38:55 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void main()
 {
-    int base, exponent;
-    long long result = 1;
+    int base,ex;
+    long long result=1;
     clrscr();
     printf("Enter a base number: ");
     scanf("%d", &base);
 
     printf("Enter an exponent: ");
-    scanf("%d", &exponent);
+    scanf("%d", &ex);
 
-    while (exponent != 0)
+    while (ex!= 0)
     {
         result *= base;
     }
-    printf("Answer = %lld", result);
+    printf("Answer = %d", result);
     getch();
 }
