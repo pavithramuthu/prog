172eee438e9b6f7ae5cecfe8d41ca1db8cece240
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-22 16:26:03 +0530

    Update armsr.c

--- armsr.c
+++ armsr.c
@@ -1,45 +1,29 @@
-#include <stdio.h>
-#include <math.h>
-
-void main()
+#include<stdio.h>
+#include<math.h>
+int main()
 {
-    int num, lastDigit, digits, sum, i;
-    int start, end;
-
-   clrscr();
-    printf("Enter lower limit: ");
-    scanf("%d", &start);
-    printf("Enter upper limit: ");
-    scanf("%d", &end);
-
+    int num,last,digits,sum,i;
+    int start,end;
+    printf("Enter lower limit:");
+    scanf("%d",&start);
+    printf("Enter upper limit:");
+    scanf("%d",&end);
     printf("Armstrong number between %d to %d are: \n", start, end);
-
-    for(i=start; i<=end; i++)
+    for(i=start;i<=end;i++)
     {
-        sum = 0;
-
-                num = i;
-
-                digits = (int) log10(num) + 1;
-
-                while(num > 0)
+        sum=0;
+                num=i;
+                digits=(int)log10(num)+1;
+                while(num>0)
         {
-            
-            lastDigit = num % 10;
-
-            
-            sum = sum + pow(lastDigit, digits);
-
-            
-            num = num / 10;
+            last=num%10;     
+            sum=sum+pow(last,digits);       
+            num=num/10;
         }
-
-        
-        if(i == sum)
+        if(i==sum)
         {
-            printf("%d, ", i);
+            printf("%d,",i);
         }
     }
-
-    getch():
+ return 0;
 }
