bc660b73cb9878c2ed35f5c3c464c6fa041dcc2b
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-22 16:59:57 +0530

    Rename new.c to new1.c

---
+++ new1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int hr1,hr2,min1,min2;
+	int hr,min;
+	printf("enter the hr1 and hr2");
+	scanf("%d%d",&hr1,&hr2);
+	printf("enter the min1 and min2");
+	scanf("%d%d",&min1,&min2);
+	hr=hr1-hr2;
+	min=min1-min2;
+	hr=hr/60;
+	
+	
+	
+}
