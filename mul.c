ecbfc4fd735d7671ab26e53928e59165b27d22c1
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-22 16:53:08 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the number\n");
+scanf("%d",&n);
+printf("multiplies are ");
+for(i=1;i<=25;i++)
+{
+if(i%5==0)
+printf("%d\n",i);
+}
+return 0;
+}
