dde452274bb63005bcc0849e1de627015abd7b59
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-24 16:10:45 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int hr,min;
+	printf("enter the min\n");
+	scanf("%d",&min);
+            hr=min/60;
+            min=min%60;
+	printf("%d %d",hr,min);
+	return 0;
+	
+	
+	
+}
