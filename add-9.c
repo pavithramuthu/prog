6a98b07e5eca4aef8de65a1bd740f99dc29e46a2
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-31 16:02:49 +0530

    Create add-9.c

---
+++ add-9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],n,k1;
+	int k2[100],i,j,k;
+	int c[200];
+            printf("enter the size");
+            scanf("%d%d",&n,&k1);
+            printf("\nenter the numbers");
+            for(i=0;i<n;i++)
+            {
+            	for(j=0;j<0;j++)
+            	{
+            		scanf("%d%d",&a[i],&k2[j]);
+            		while(n==k1)
+            		{
+            			c[k]=a[i]+k2[j];
+            			printf("sum is%d",c[k]);
+            		}
+            	}
+            }
+            return 0;
+}
