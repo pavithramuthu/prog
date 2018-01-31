157e0eed343b7883db4901b87b6ce224a2023add
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-31 16:55:49 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int ele[20],size,i,min,max;
+	printf("Enter the size");
+	scanf("%d",&size);
+	printf("\nEnter the elements:");
+	for(i=0;i<size;i++)
+	{
+	scanf("%d",&ele[i]);
+	}
+	max=min=ele[0];
+	for(i=0;i<size;i++)
+	{
+		if(ele[i]<min)
+		{
+		min=ele[i];		}
+			printf("\nmin=%d",min);
+		
+	}
+		
+	return 0;
+	
+}
