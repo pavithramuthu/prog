0d293bea7d1006c3783c71198ad5dea4f48fb959
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-01-31 16:50:29 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int ele[20],size,i,max,min;
+	printf("Enter the size");
+	scanf("%d",&size);
+	printf("\nEnter the elements:");
+	for(i=0;i<size;i++)
+	{
+	scanf("%d",&ele[i]);
+	}
+	max=min=ele[0];
+	for(i=0;i<size;i++)
+	{
+		if(ele[i]>max)
+		{
+		max=ele[i];
+		}
+		else
+		{
+				}
+			printf("\nmax=%d",max);
+			}
+		
+	
+	
+}
