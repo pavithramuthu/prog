93622a01cce19264da38581a01f33cc0cddf255e
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-02 16:02:01 +0530

    oddrange

--- oddrange.c
+++ oddrange.c
@@ -7,7 +7,7 @@ int main()
   printf("All odd numbers from %d to %d are:\n",m,n);
    for(i=m;i<n;i++)
     {
-               if(i%2!=0)
+              if(i%2!=0)
         {
             printf("%d\n",i);
         }
