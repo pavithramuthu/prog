fd2f7d6a11e16ff7a3034de6a43027d206d8a731
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-02 15:57:55 +0530

    time

--- time.c
+++ time.c
@@ -4,8 +4,8 @@ int main()
 	int hr,min;
 	printf("enter the min\n");
 	scanf("%d",&min);
-            hr=min/60;
-            min=min%60;
+         hr=min/60;
+         min=min%60;
 	printf("%d %d",hr,min);
 	return 0;
 	
