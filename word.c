164a55ab53bab42489641d5d3ed19ef2edaaf445
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-09 16:29:16 +0530

    words

---
+++ word.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char s[50];
+	int c=1,i;
+	printf("\n Enter your string:");
+	scanf("%[^\n]",&s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]==' ')
+		{
+
+			c=c+1;
+	}
+	}
+	printf("\n The number of spaces are%d:",c);
+	return 0;
+}
