36f5f3f3236bb744670e214c442c90e942bae683
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-19 15:54:23 +0530

    maximumof3

---
+++ max10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int ele[10],i,min,max;
+	
+	printf("\nEnter the elements:");
+	for(i=0;i<10;i++)
+	{
+	scanf("%d",&ele[i]);
+	}
+	max=min=ele[0];
+	for(i=0;i<;i++)
+	{
+		if(ele[i]>min)
+		{
+		min=ele[i];		}
+			
+		
+	}
+printf("\nmax=%d",min);
+	return 0;
+	
+}
