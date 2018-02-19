1b6e8c5c15c721285752ba9550924ef11580078c
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-19 16:30:20 +0530

    onetoten

---
+++ 1to10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number");
+	scanf("%d",&n);
+	if((1<n)&&(n<10))
+	{
+		printf("\nYES");
+	}
+	else
+	{
+		printf("\nNO");
+	}
+	
+	return 0;
+	
+}
