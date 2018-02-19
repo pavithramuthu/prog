3c5590fd1c7c21e45d2b6326e406f8c5299c550f
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-19 16:17:39 +0530

    minmaxsum

---
+++ minmaxsum.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int ele[1000],i,size,min,max;
+	printf("\nEnter the size");
+scanf("%d",&size);
+	printf("\nEnter the elements:");
+	for(i=0;i<size;i++)
+	{
+	scanf("%d",&ele[i]);
+	}
+	max=min=ele[0];
+	for(i=0;i<size;i++)
+	{
+		if(ele[i]>min)
+		{
+		max=ele[i];		}
+			
+		
+	}
+printf("\nmax=%d",max);
+{
+for(i=0;i<size;i++)
+	{
+		if(ele[i]<min)
+		{
+		min=ele[i];		}
+			
+		
+	}
+printf("\nmin=%d",min);
+printf("\n%d%d",min,max);
+	return 0;
+}	
+}
