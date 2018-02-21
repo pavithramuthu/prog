6023fc80d50f1e6ad9f115696f2d787a1f265ea5
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-21 15:50:21 +0530

    MAXIMUM NUMBER

---
+++ MAXIMUM.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int ele[10],i,min,max;
+	printf("\nEnter the elements:");
+	for(i=0;i<10;i++)
+	{
+	scanf("%d",&ele[i]);
+	}
+	max=min=ele[0];
+	for(i=0;i<10;i++)
+	{
+		if(ele[i]>min)
+		{
+		min=ele[i];		}
+	}
+printf("\nmax=%d",min);
+	return 0;
+	
+}
