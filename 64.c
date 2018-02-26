779d60932772727bcfec53b97326b88315a51ca8
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-02-26 16:06:48 +0530

    sum_odd/even

---
+++ 64.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the number");
+scanf("%d%d",&a,&b);
+c=a+b;
+if(c%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("ODD");
+}
+getch();
+}
