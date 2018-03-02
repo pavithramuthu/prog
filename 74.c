d13eab759f569cb8424c1ce9a58eefa9169b305d
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-03-02 16:46:21 +0530

    74

---
+++ 74.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float n;
+int m;
+printf("Enter the float value");
+scanf("%f",&n);
+m=n;
+printf("\nThe next greater nearest number is %d",m+1);
+getch();
+}
