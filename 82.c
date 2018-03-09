dc8e7ef6ae0cda770a4f7571b64da83c49bf9cc4
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-03-09 15:45:21 +0530

    82

---
+++ 82.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,l,b;
+clrscr();
+printf("Enter the values of length and breadth");
+scanf("%f%f",&l,&b);
+a=l*b;
+printf("\nThe ans is %0.5f",a);
+getch();
+}
