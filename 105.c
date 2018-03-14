910da6d21c9fe300dfa791267f306d06984cf26c
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-03-14 16:52:34 +0530

    105

---
+++ 105.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+ {
+	int n;
+	char a='A';
+	clrscr();
+	printf("enter input");
+	scanf("%d",&n);
+	a=a+n-1;
+	printf("\nout put=%c",a);
+	getch();
+ }
