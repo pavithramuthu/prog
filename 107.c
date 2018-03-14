04784ca44c60128d8d46f221af3b4542b59dd315
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-03-14 16:14:15 +0530

    107

---
+++ 107.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+printf("Enter the values of a,b,c:");
+scanf("%d%d%d",&a,&b,&c);
+d=(a*b)/c;
+printf("\nThe value is %d",d);
+getch();
+}
