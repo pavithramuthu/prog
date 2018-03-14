5103ff4b8faed3abb4abfab33c91bee669f97b60
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-03-14 16:44:12 +0530

    corresponding input

---
+++ 106.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+ {
+	int n;
+	char s='A';
+	clrscr();
+	printf("enter input");
+	scanf("%d",&n);
+	s=s+n-1;
+	printf("\nout put=%c",s);
+	getch();
+ }
