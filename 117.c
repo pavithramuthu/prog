13a260b92e9a5ff5d89511889a5d186360340e6c
pavithramuthu <30549707+pavithramuthu@users.noreply.github.com>
2018-03-16 15:52:58 +0530

    117

---
+++ 117.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m;
+clrscr();
+printf("Enter the number n and m");
+scanf("%d%d",&n,&m);
+printf("\nAdding of %d and %d is given as %d%d",n,m,n,m);
+getch();
+}
